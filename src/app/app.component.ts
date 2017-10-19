1848359da9839ee6eca1253fd0cb0836490cd365
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-10-19 14:31:21 +0200

    Añadida funcionalidad de clickado y modificación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,22 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li 
+        *ngFor="let hero of heroes" 
+        (click)="onSelect(hero)"
+        [class.selected]="hero === selectedHero"
+      >
       <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+          <label>name: </label>
+          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
   `,
   styles: [`
     .selected {
@@ -77,11 +89,11 @@ const HEROES: Hero[] = [
 export class AppComponent  { 
   name = 'Angular';
   title = 'Hero Editor';
-  hero: Hero = {
-    id: 1,
-    name: 'Bakugo'
-  }
+  selectedHero : Hero;
   heroes = HEROES;
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero{
