5a87da74105f4e9fb1681b949dca45c011429368
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-10-19 14:20:31 +0200

    Añadida lista de heroes y estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,78 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+  { id: 1, name: 'Bakugo' },
+  { id: 2, name: 'Tenya' },
+  { id: 3, name: 'Deku' },
+  { id: 4, name: 'Frogi' },
+  { id: 5, name: 'Zelo' },
+  { id: 6, name: 'All Might' },
+  { id: 7, name: 'Steins' },
+  { id: 8, name: 'Magnus' },
+  { id: 9, name: 'Vault-boy' },
+  { id: 10, name: 'Batman' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
   `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent  { 
   name = 'Angular';
@@ -19,6 +81,7 @@ export class AppComponent  {
     id: 1,
     name: 'Bakugo'
   }
+  heroes = HEROES;
 }
 
 export class Hero{
