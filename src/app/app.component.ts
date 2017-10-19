c071094fb4d6c67820c90a4b8e6fbf5a3a2ebb11
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-10-19 15:11:41 +0200

    Refactorizado el código separando clases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 1, name: 'Bakugo' },
@@ -27,14 +28,7 @@ const HEROES: Hero[] = [
       <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
     .selected {
@@ -94,9 +88,4 @@ export class AppComponent  {
   onSelect(hero:Hero): void{
     this.selectedHero = hero;
   }
-}
-
-export class Hero{
-  id: number;
-  name: string;
 }
