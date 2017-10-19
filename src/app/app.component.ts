bf61b00d2eb0ea6563ed868ef65d1e4b988a8afb
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-10-19 13:53:34 +0200

    Primer acercamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular';
+  title = 'Hero Editor';
+  hero: Hero = {
+    id: 1,
+    name: 'Bakugo'
+  }
+}
+
+export class Hero{
+  id: number;
+  name: string;
+}
