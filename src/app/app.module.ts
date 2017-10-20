73aec5094a100df403724fa5b42f56c6a09b747c
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-10-20 12:01:50 +0200

    Añadido dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService }         from './hero.service';
 
 
@@ -17,10 +18,23 @@ import { HeroService }         from './hero.service';
         {
           path: 'heroes',
           component: HeroesComponent
+        },
+        {
+          path: 'dashboard',
+          component: DashboardComponent
+        },
+        {
+          path: '',
+          redirectTo: '/dashboard',
+          pathMatch: 'full'
+        },
+        {
+          path: 'detail/:id',
+          component: HeroDetailComponent
         }
       ])
   ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
